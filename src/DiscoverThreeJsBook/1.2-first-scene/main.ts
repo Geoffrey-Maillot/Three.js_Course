d1d3dd2828e00d0286d68c0d32bb66765bbff890
geoffrey <geoffrey.maillot@gmail.com>
2023-10-08 17:49:03 +0200

    first commit

---
+++ src/DiscoverThreeJsBook/1.2-first-scene/main.ts
@@ -0,0 +1,12 @@
+import {
+  BoxGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+
+// Get a reference to the container element that will hold our scene
+const container = document.querySelector("#app") as HTMLDivElement;
---
+++ src/ThreeJsJourney/03-basic-scene/main.ts
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+// Sizes
+const sizes = {
+  with: 800,
+  height: 600,
+  aspectRatio: function () {
+    return this.with / this.height;
+  },
+};
+
+// Get Canvas ELement
+const canvas = document.querySelector('.webgl') as HTMLCanvasElement;
+
+// Create Scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 'red' });
+const mesh = new THREE.Mesh(geometry, material);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.aspectRatio());
+camera.position.z = 3;
+camera.position.x = 1;
+
+// Add mesh in the scene
+scene.add(mesh);
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+
+renderer.setSize(sizes.with, sizes.height);
+
+// Render
+renderer.render(scene, camera);
